fb7f48daddfe33b19ce087b04e13740a8e5b9241
alexthegreat194 <alexander.gentry@aol.com>
2022-02-16 16:09:37 -0800

    finished shopping list

--- grocery_app/models.py
+++ grocery_app/models.py
@@ -3,6 +3,8 @@ from sqlalchemy_utils import URLType
 from grocery_app.extensions import db
 from grocery_app.utils import FormEnum
 
+from flask_login import UserMixin
+
 class ItemCategory(FormEnum):
     """Categories of grocery items."""
     PRODUCE = 'Produce'
@@ -14,18 +16,41 @@ class ItemCategory(FormEnum):
 
 class GroceryStore(db.Model):
     """Grocery Store model."""
+    __tablename__ = 'GroceryStore'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), nullable=False)
     address = db.Column(db.String(200), nullable=False)
     items = db.relationship('GroceryItem', back_populates='store')
-
+    
+    created_by_id = db.Column(db.Integer, db.ForeignKey('User.id'))
+    created_by = db.relationship('User')
+    
 class GroceryItem(db.Model):
     """Grocery Item model."""
+    __tablename__ = 'GroceryItem'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     price = db.Column(db.Float(precision=2), nullable=False)
     category = db.Column(db.Enum(ItemCategory), default=ItemCategory.OTHER)
     photo_url = db.Column(URLType)
     store_id = db.Column(
-        db.Integer, db.ForeignKey('grocery_store.id'), nullable=False)
+        db.Integer, db.ForeignKey('GroceryStore.id'), nullable=False)
     store = db.relationship('GroceryStore', back_populates='items')
+    
+    created_by_id = db.Column(db.Integer, db.ForeignKey('User.id'))
+    created_by = db.relationship('User')    
+    
+    carts = db.relationship('User', secondary='shopping_list_items', back_populates='shopping_list_items')
+    
+class User(db.Model, UserMixin):
+    __tablename__ = 'User'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String())
+    password = db.Column(db.String())
+    
+    shopping_list_items = db.relationship('GroceryItem', secondary='shopping_list_items', back_populates='carts')
+    
+shopping_list_table = db.Table('shopping_list_items', 
+    db.Column('user', db.Integer, db.ForeignKey('User.id')), 
+    db.Column('item', db.Integer, db.ForeignKey('GroceryItem.id')),
+)
--- grocery_app/routes.py
+++ grocery_app/routes.py
@@ -1,11 +1,14 @@
 from unicodedata import category
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from datetime import date, datetime
-from grocery_app.models import GroceryStore, GroceryItem
-from grocery_app.forms import GroceryStoreForm, GroceryItemForm
+
+import flask_login
+from grocery_app.models import GroceryStore, GroceryItem, User
+from grocery_app.forms import GroceryStoreForm, GroceryItemForm, SignUpForm, LoginForm
 
 # Import app and db from events_app package so that we can run app
-from grocery_app.extensions import app, db
+from grocery_app.extensions import app, db, bcrypt, login_manager
+from flask_login import login_user, logout_user, current_user, login_required
 
 main = Blueprint("main", __name__)
 
@@ -20,6 +23,7 @@ def homepage():
     return render_template('home.html', all_stores=all_stores)
 
 @main.route('/new_store', methods=['GET', 'POST'])
+@login_required
 def new_store():
     # TODO: Create a GroceryStoreForm
     form = GroceryStoreForm()
@@ -31,7 +35,8 @@ def new_store():
     if form.validate_on_submit():
         store = GroceryStore(
             title = form.title.data,
-            address = form.address.data
+            address = form.address.data,
+            created_by = flask_login.current_user
         )
         db.session.add(store)
         db.session.commit()
@@ -42,6 +47,7 @@ def new_store():
     return render_template('new_store.html', form=form)
 
 @main.route('/new_item', methods=['GET', 'POST'])
+@login_required
 def new_item():
     # TODO: Create a GroceryItemForm
     form = GroceryItemForm()
@@ -56,7 +62,8 @@ def new_item():
             price = form.price.data,
             category = form.category.data,
             photo_url = form.photo_url.data,
-            store = form.store.data
+            store = form.store.data,
+            created_by = flask_login.current_user
         )
         db.session.add(item)
         db.session.commit()
@@ -67,6 +74,7 @@ def new_item():
     return render_template('new_item.html', form=form)
 
 @main.route('/store/<store_id>', methods=['GET', 'POST'])
+@login_required
 def store_detail(store_id):
     store = GroceryStore.query.get(store_id)
     # TODO: Create a GroceryStoreForm and pass in `obj=store`
@@ -88,6 +96,7 @@ def store_detail(store_id):
     return render_template('store_detail.html', store=store, form=form)
 
 @main.route('/item/<item_id>', methods=['GET', 'POST'])
+@login_required
 def item_detail(item_id):
     item = GroceryItem.query.get(item_id)
     # TODO: Create a GroceryItemForm and pass in `obj=item`
@@ -112,3 +121,63 @@ def item_detail(item_id):
     item = GroceryItem.query.get(item_id)
     return render_template('item_detail.html', item=item, form=form)
 
+@main.route('/add_to_shopping_list/<item_id>', methods=['POST'])
+@login_required
+def add_to_shopping_list(item_id):
+    user = flask_login.current_user
+    item = GroceryItem.query.get(item_id)
+    user.shopping_list_items.append(item)
+    db.session.commit()
+    print(user.shopping_list_items)
+    return redirect(url_for('main.shopping_list'))
+
+@main.route('/shopping_list')
+@login_required
+def shopping_list():
+    # ... get logged in user's shopping list items ...
+    user = flask_login.current_user
+    items = user.shopping_list_items
+    print(user, items)
+    # ... display shopping list items in a template ...
+    return render_template('shopping_list.html', items=items)
+
+# routes.py
+
+auth = Blueprint("auth", __name__)
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    print('in signup')
+    form = SignUpForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(
+            username=form.username.data,
+            password=hashed_password
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash('Account Created.')
+        print('created')
+        
+        return redirect(url_for('main.homepage'))
+    print(form.errors)
+    print('back to signup')
+    return render_template('signup.html', form=form)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        login_user(user, remember=True)
+        next_page = request.args.get('next')
+        return redirect(next_page if next_page else url_for('main.homepage'))
+    return render_template('login.html', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.homepage'))
